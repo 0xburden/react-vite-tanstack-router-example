d6f27314516fa98f0440ec7771c131872dd78811
0xburden <0xburden@protonmail.com>
2025-04-02 22:27:48 -0700

    create app and add tailwindcss, remove boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-w-screen min-h-screen bg-indigo-600 text-white flex flex-col gap-4 items-center justify-center">
+      <h1 className="text-2xl font-bold">Vite + React</h1>
+      <p className="text-sm">And some other stuff</p>
+    </div>
+  );
+}
+
+export default App;
