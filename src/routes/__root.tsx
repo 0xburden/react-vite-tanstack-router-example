ac0fb77a5bb24a65482130e0ffafccc98f47dbc3
0xburden <0xburden@protonmail.com>
2025-04-02 23:02:07 -0700

    add react query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,25 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryProvider } from "../modules/global/providers";
 import "../index.css";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="min-w-screen min-h-screen bg-indigo-600 text-white">
-      <div className="p-4 flex items-center gap-2 bg-indigo-900">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/other" className="[&.active]:font-bold">
-          Other
-        </Link>
+    <QueryProvider>
+      <div className="min-w-screen min-h-screen bg-indigo-600 text-white">
+        <div className="p-4 flex items-center gap-2 bg-indigo-900">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{" "}
+          <Link to="/other" className="[&.active]:font-bold">
+            Other
+          </Link>
+        </div>
+        <Outlet />
       </div>
-      <Outlet />
       <TanStackRouterDevtools />
-    </div>
+      <ReactQueryDevtools />
+    </QueryProvider>
   ),
 });
